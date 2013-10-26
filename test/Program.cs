6f60342e0b9ff192eacb2c08ca3db3970f21081e
maroambro <maroambros@gmail.com>
2013-10-26 14:59:24 +0200

    test

---
+++ test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            string t;
+            Console.Write("x: ");
+            t = Console.ReadLine();
+            x = int.Parse(t);
+
+            Console.Write("y: ");
+            t = Console.ReadLine();
+            y = int.Parse(t);
+
+            if (x > y)
+                Console.WriteLine("{0}>{1}", x, y);
+            else if (x < y)
+                Console.WriteLine("{0}<{1}", x, y);
+            else
+                Console.WriteLine("{0}={1}", x, y);
+
+            Console.ReadLine();
+        }
+    }
+}
